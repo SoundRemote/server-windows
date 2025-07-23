4f7c3e3ce4e1c3db0f8ca02f11bbbbc87df46eaa
ashipo <24320267+ashipo@users.noreply.github.com>
2025-07-23 18:20:06 +0300

    Refactor Settings.cpp

--- SoundRemote/Settings.cpp
+++ SoundRemote/Settings.cpp
@@ -3,22 +3,24 @@
 #include "NetDefines.h"
 
 namespace Section {
-	const wchar_t* network{ L"network" };
-	const wchar_t* general{ L"general" };
+	constexpr auto network{ L"network" };
+	constexpr auto general{ L"general" };
 }
 
 namespace Setting {
-	const wchar_t* serverPort{ L"server_port" };
-	const wchar_t* clientPort{ L"client_port" };
-	const wchar_t* checkUpdates{ L"check_updates" };
+	constexpr auto serverPort{ L"server_port" };
+	constexpr auto clientPort{ L"client_port" };
+	constexpr auto checkUpdates{ L"check_updates" };
 }
 
 namespace DefaultValue {
+	constexpr auto serverPort{ Net::defaultServerPort };
+	constexpr auto clientPort{ Net::defaultClientPort };
 	constexpr bool checkUpdates{ true };
 }
 
 Settings::Settings(const std::string& fileName): fileName_(fileName) {
-	ini_ = std::make_unique<CSimpleIniCaseW>();
+	ini_ = std::make_unique<CSimpleIniCaseW>(true);
 	SI_Error rc = ini_->LoadFile(fileName.c_str());
 	if (rc == SI_OK) {
 		checkMissingSettings();
@@ -46,8 +48,8 @@ void Settings::setCheckUpdates(bool check) {
 }
 
 void Settings::setDefaultValues() {
-	ini_->SetLongValue(Section::network, Setting::serverPort, Net::defaultServerPort);
-	ini_->SetLongValue(Section::network, Setting::clientPort, Net::defaultClientPort);
+	ini_->SetLongValue(Section::network, Setting::serverPort, DefaultValue::serverPort);
+	ini_->SetLongValue(Section::network, Setting::clientPort, DefaultValue::clientPort);
 	ini_->SetBoolValue(Section::general, Setting::checkUpdates, DefaultValue::checkUpdates);
 }
 
@@ -56,14 +58,14 @@ void Settings::checkMissingSettings() {
 	if (!ini_->KeyExists(Section::network, Setting::serverPort)) {
 		// todo: remove eventually 
 		// Migrate value from version 0.5.2 or older
-		auto serverPort = ini_->GetLongValue(L"", Setting::serverPort, Net::defaultServerPort);
+		auto serverPort = ini_->GetLongValue(L"", Setting::serverPort, DefaultValue::serverPort);
 		ini_->SetLongValue(Section::network, Setting::serverPort, serverPort);
 		saveNeeded = true;
 	}
 	if (!ini_->KeyExists(Section::network, Setting::clientPort)) {
 		// todo: remove eventually 
 		// Migrate value from version 0.5.2 or older
-		auto clientPort = ini_->GetLongValue(L"", Setting::clientPort, Net::defaultClientPort);
+		auto clientPort = ini_->GetLongValue(L"", Setting::clientPort, DefaultValue::clientPort);
 		ini_->SetLongValue(Section::network, Setting::clientPort, clientPort);
 		saveNeeded = true;
 	}
